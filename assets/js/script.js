bbafc5fe0bf67656b1d282b1fd9a7fee41fe174a
mjnhnn <61675864+mjnhnn@users.noreply.github.com>
2020-04-15 11:40:10 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var element = document.getElementById("specifi-hide");
+    element.classList.toggle("opentab");
+
+    var an = document.getElementById("down");
+    an.classList.toggle("closetab");
+    var hien = document.getElementById("up");
+    hien.classList.toggle("opentab");
+}
